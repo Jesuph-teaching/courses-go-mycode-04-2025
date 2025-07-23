3fbeb2121fdbb1a7f13cdeaa464a1ae1d7c0ecd7
Youcef Madadi <tahher@live.fr>
2025-07-23 11:51:52 +0100

    feat: refactor animal interface and improve attack method implementations

--- 10.typescript/first-test/src/index.ts
+++ 10.typescript/first-test/src/index.ts
@@ -19,57 +19,70 @@ const id2: ID = 23;
 // create a type animal with the following elements : name , specie, lifeSpan
 // create 4 animal variables
 
-type Species = 'Mammals' | 'Fishes' | 'Insects' | 'Birds';
+type SpeciesT = 'Mammals' | 'Fishes' | 'Insects' | 'Birds';
 
-interface Animal<LIFESPAN extends string | number = number> {
+interface AnimalI<LIFESPAN extends string | number = number> {
 	name: string;
-	specie: Species;
+	specie: SpeciesT;
 	lifespan: LIFESPAN;
+	attack?: (animal: Pick<AnimalI, 'name'>) => void;
 }
 
-const animal1: Animal = {
+const animal1: AnimalI = {
 	name: 'Lion',
 	specie: 'Mammals',
 	lifespan: 15,
+	attack: function (animal) {
+		console.log(`The ${this.name} bit ${animal.name}`);
+	},
 };
 
-const animal2: Animal = {
+const animal2: AnimalI = {
 	name: 'Elephant',
 	specie: 'Mammals',
 	lifespan: 70,
+	attack: function (animal) {
+		console.log(`The ${this.name} hit ${animal.name} with his trunk`);
+	},
 };
 
-const animal3: Animal = {
+const animal3: AnimalI = {
 	name: 'Eagle',
 	specie: 'Birds',
 	lifespan: 25,
+	attack(animal) {
+		console.log(`The ${this.name} picked the ${animal.name}`);
+	},
 };
-
-const animal4: Animal = {
+const animal4: AnimalI = {
 	name: 'Shark',
 	specie: 'Fishes',
 	lifespan: 30,
 };
-const animal5: Animal<`${number} days`> = {
+const animal5: AnimalI<`${number} days`> = {
 	name: 'mosquito',
 	specie: 'Insects',
 	lifespan: '10 days',
 };
 // removing the keys mentioned
-const unknownAnimal: Omit<Animal, 'lifespan'> = {
+const unknownAnimal: Omit<AnimalI, 'lifespan'> = {
 	name: 'Roc',
 	specie: 'Birds',
 };
 // making keys optional
-const dragon: Partial<Animal> = {
+const dragon: Partial<AnimalI> = {
 	name: 'Dragon',
 };
 
-const crocodile: Pick<Animal, 'name' | 'lifespan'> = {
+const crocodile: Pick<AnimalI, 'name' | 'lifespan'> = {
 	name: 'crocodile',
 	lifespan: 15,
 };
-
+animal1.attack!(animal2); // ! obligatory calling (can cause an error if element doesn't exist)
+animal2.attack?.(crocodile); // ?. optional calling the function/ attribute
+animal3.attack?.(crocodile);
+animal4.attack?.(animal1); // not gonna be called because its optional
+//animal4.attack!(animal1); // gonna be called and cause an error
 console.log({
 	animal1,
 	animal2,
